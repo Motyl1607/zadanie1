37d1bcaf32501152b081b136b9035e9ca71a8010
Lukasz Motylewski <motylek-foto@wp.pl>
2015-10-15 11:29:21 +0200

    drugi commit

--- JavaApplication.java
+++ JavaApplication.java
@@ -15,7 +15,8 @@ public class JavaApplication {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-       System.out.println("My name is Lukasz");
+       System.out.println("drugi commit");
+       System.out.println("powoli zaczynam rozumiec");
     }
     
 }

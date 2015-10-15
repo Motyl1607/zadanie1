1d7e37a2294dd16b05d763392b63137e7a66720a
Lukasz Motylewski <motylek-foto@wp.pl>
2015-10-15 09:44:17 +0200

    second repository

--- JavaApplication.java
+++ JavaApplication.java
@@ -15,7 +15,7 @@ public class JavaApplication {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-       System.out.println("Hello Word");
+       System.out.println("My name is Lukasz");
     }
     
 }
